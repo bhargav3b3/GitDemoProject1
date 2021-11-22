ee9d54078071cd8240b3fc222b91746c369b89b5
bhargav3b3@gmail.com <bhargav3b3@gmail.com>
2021-11-22 23:43:29 +0530

    changes done for greetings

--- src/com/FirstGitHubProj/group1/Greet.java
+++ src/com/FirstGitHubProj/group1/Greet.java
@@ -3,7 +3,7 @@ package com.FirstGitHubProj.group1;
 public class Greet {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to github");
+		System.out.println("welcome to the github");
 	}
 
 }
